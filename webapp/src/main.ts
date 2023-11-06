d80084585632ac4ce83da15d549313efd294c004
Tanya Fomina <fomina.tatianaaa@yandex.ru>
2023-11-06 21:09:03 +0300

    Add yandex metrika

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -3,11 +3,23 @@ import './style.css';
 import App from './App.vue';
 import { router } from './router';
 import { i18n } from './i18n';
+// @ts-expect-error no types for the package
+import VueYandexMetrika from 'vue3-yandex-metrika';
 
 const app = createApp(App);
 
 app.use(router);
 
 app.use(i18n);
+
+if (import.meta.env.VITE_YA_METRIKA_ID !== undefined) {
+  app.use(VueYandexMetrika, {
+    id: import.meta.env.VITE_YA_METRIKA_ID,
+    router,
+    // @ts-expect-error no types
+    env: process.env.NODE_ENV,
+    // other options
+  });
+}
 
 app.mount('#app');
